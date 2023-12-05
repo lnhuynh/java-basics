bc7538717d109fd70275d007bc996486ae121891
Huỳnh Lê <lnhuynhtma@gmail.com>
2023-12-05 10:26:28 +0700

    Add DI example

---
+++ src/javaadvanced/DependenciesInjection.java
@@ -0,0 +1,43 @@
+package javaadvanced;
+
+class Pizza {
+
+}
+
+class Chef {
+  public Pizza pizza;
+//  private Pizza pizza;
+  //Without DI
+  Chef() {
+    Pizza pizza = new Pizza();
+  }
+
+  //With DI
+  Chef(Pizza pizza) {
+    this.pizza = pizza;
+  }
+
+  public void setPizza(Pizza pizza) {
+    this.pizza = pizza;
+  }
+}
+
+public class DependenciesInjection {
+  public static void main(String[] args) {
+    //3 types of DI
+    Pizza pizza = new Pizza();
+
+    //Constructor injection
+    Chef chef1 =  new Chef(pizza);
+
+    //Setter injection
+    Chef chef2 = new Chef();
+    chef2.setPizza(pizza);
+
+    //Field injection
+    Chef chef3 = new Chef();
+    chef3.pizza = pizza;
+
+  }
+
+}
